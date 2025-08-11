1bffe30cde62cc06cbd9b8f998b24067e580875b
David Vigh <david.vigh@aviditybio.com>
2025-08-11 13:03:02 -0500

    change openwebui to avidgpt

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -106,9 +106,9 @@ for source in log_sources:
 
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "Tier 1 AvidGPT")
+if WEBUI_NAME != "Tier 1 AvidGPT":
+    WEBUI_NAME += " (Tier 1 AvidGPT)")
 
 WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
 
